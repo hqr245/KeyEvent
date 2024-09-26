fba78395b18ffeb1384474e05044666878f68574
Antonio Caro <antoniojosecaropozo@gmail.com>
2024-09-26 10:12:05 +0200

    feat: main class

--- src/main/java/es/ieslosmontecillos/keyevent/KeyEvent.java
+++ src/main/java/es/ieslosmontecillos/keyevent/KeyEvent.java
@@ -1,7 +1,6 @@
 package es.ieslosmontecillos.keyevent;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -11,7 +10,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class KeyEvent extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -34,13 +33,17 @@ public class HelloApplication extends Application {
 
             switch (kC){
                 case UP:
-                    text.
+                   text.setTranslateY(text.getTranslateY()-10);
+                    System.out.println(text.getTranslateY());
                     break;
                     case DOWN:
+                        text.setTranslateY(text.getTranslateY()+10);
                         break;
                         case LEFT:
+                            text.setTranslateX(text.getTranslateX()-10);
                             break;
                             case RIGHT:
+                                text.setTranslateX(text.getTranslateX()+10);
                                 break;
                                 default:
                                     event.consume();
